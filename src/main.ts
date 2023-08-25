1036901f3cad9fb37303697c11b950e03240a17c
L.F. Severgnini <luissevergnini@gmail.com>
2023-08-25 12:58:32 -0300

    chore: add more instructions to prompt

--- src/main.ts
+++ src/main.ts
@@ -101,13 +101,16 @@ async function getBaseAndHeadShas(
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
   return `Your task is to review pull requests (PR). Instructions:
 - Provide the response in following JSON format:  [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]
-- Do not give positive comments or compliments.
+- DO NOT give positive comments or compliments.
+- DO NOT give advice on renaming variable names or writing more descriptive variables.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise return an empty array.
 - Provide at most ${REVIEW_MAX_COMMENTS} comments. It's up to you how to decide which comments to include.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 ${REVIEW_PROJECT_CONTEXT ? `- Additional context regarding this PR's project: ${REVIEW_PROJECT_CONTEXT}` : ""}
 - IMPORTANT: NEVER suggest adding comments to the code.
+- IMPORTANT: Evaluate the entire diff in the file before adding any comments.
+- IMPORTANT: Consider that you might have been given only a chunk of a file, so DO NOT assume by default that attributes or methods don't exist.
 
 Review the following code diff in the file "${
     file.to
