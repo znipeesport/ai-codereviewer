3f91be334a50d6d8cc61f7bf1af5a9405b31255c
L.F. Severgnini <luis@demand.io>
2023-09-19 12:31:46 -0300

    chore: bump max tokens

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ const OPENAI_API_MODEL: string = core.getInput("OPENAI_API_MODEL");
 const REVIEW_MAX_COMMENTS: string = core.getInput("REVIEW_MAX_COMMENTS");
 const REVIEW_PROJECT_CONTEXT: string = core.getInput("REVIEW_PROJECT_CONTEXT");
 
+const MAX_TOKENS = 32768;
+
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
 const configuration = new Configuration({
@@ -151,7 +153,7 @@ async function getAIResponse(
   const queryConfig = {
     model: OPENAI_API_MODEL,
     temperature: 0.2,
-    max_tokens: 700,
+    max_tokens: MAX_TOKENS,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
